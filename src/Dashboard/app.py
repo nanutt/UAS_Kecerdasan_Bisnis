5339738d6497c12798c53ae83c527e0f72b60ef2
nanutt <najahmaisya03@gmail.com>
2025-12-22 11:24:58 +0800

    update

--- src/Dashboard/app.py
+++ src/Dashboard/app.py
@@ -863,7 +863,7 @@ def main_app():
             col1, col2 = st.columns([2, 1.3])
             with col1:
                 st.markdown("""
-                        <div style="background-color: #044335; border-radius: 15px 15px 0 0; padding: 20px 25px 15px 25px; margin-right: -179px; margin-bottom: -20px;">
+                        <div style="background-color: #044335; border-radius: 0; padding: 20px 25px 15px 25px; margin-right: -173px; margin-bottom: -20px;">
                             <h4 style="color: white; font-family: Poppins; margin: 0; padding: 0;">Kabupaten/Kota</h4>
                         </div>
                     """, unsafe_allow_html=True
@@ -1059,7 +1059,7 @@ def main_app():
                 <div style="background-color: black; padding: 10px; border-radius: 8px; 
                             box-shadow: 0 4px 10px rgba(0,0,0,0.1); margin-top: -10px;
                             font-family: 'Poppins', sans-serif; font-size: 0.8rem;">
-                    <h5 style="margin: 0 0 5px 0; color: #044335; text-align: center;">Legenda Beban Kerja</h5>
+                    <h5 style="margin: 0 0 5px 0; color: white; text-align: center;">Legenda Beban Kerja</h5>
                     <div style="display: flex; justify-content: space-around;">
                         <div style="display: flex; align-items: center; gap: 5px;">
                             <span style="color: #4ade80; font-size: 1.5rem;">●</span> Rendah (≤ 8:1)
